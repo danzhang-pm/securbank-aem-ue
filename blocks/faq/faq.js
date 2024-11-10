5df85555a3edc47b0612039e343b5aa23da6b619
dan.zhang <54833095+danzhang-pm@users.noreply.github.com>
2024-11-10 09:38:14 +0800

    Update faq.js

--- blocks/faq/faq.js
+++ blocks/faq/faq.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-underscore-dangle */
 export default async function decorate(block) {
   const props = [...block.children];
-  const firsttag = props[0].textContent.trim();
+  const firsttag = props[0].textContent.trim() || 'faqList';
   const variationname = props[1].textContent.trim() || 'master';
   const cachebuster = Math.floor(Math.random() * 1000);
 
